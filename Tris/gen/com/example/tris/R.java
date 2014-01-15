3ad1a8711c89ab98d497041c019cdff21ad1e519
empy92 <empy92@hotmail.it>
2014-01-15 14:14:52 +0100

    add basic layout

--- Tris/gen/com/example/tris/R.java
+++ Tris/gen/com/example/tris/R.java
@@ -22,9 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int o=0x7f020001;
+        public static final int x=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int buttonStartGame=0x7f080000;
     }
     public static final class layout {
         public static final int activity_grid=0x7f030000;
@@ -35,6 +38,7 @@ public final class R {
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int StartGame=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int title_activity_grid=0x7f050003;
