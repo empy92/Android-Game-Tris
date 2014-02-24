792ed46f2aaa7d23723a908371b32dd41857affd
CaoS <sosi.andrea@gmail.com>
2014-02-24 14:48:45 +0100

    Some fix, add settings activity

--- Tris/gen/com/example/tris/R.java
+++ Tris/gen/com/example/tris/R.java
@@ -27,7 +27,7 @@ public final class R {
         public static final int x=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080010;
         public static final int b00=0x7f080000;
         public static final int b01=0x7f080001;
         public static final int b02=0x7f080002;
@@ -37,32 +37,37 @@ public final class R {
         public static final int b20=0x7f080006;
         public static final int b21=0x7f080007;
         public static final int b22=0x7f080008;
-        public static final int button1=0x7f08000c;
         public static final int imgLogo=0x7f08000d;
+        public static final int settingsButton=0x7f08000c;
         public static final int startGame1v1=0x7f08000b;
         public static final int startGame1vIA=0x7f08000a;
+        public static final int textView1=0x7f08000e;
+        public static final int textView2=0x7f08000f;
         public static final int todo=0x7f080009;
     }
     public static final class layout {
         public static final int activity_grid=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_splash=0x7f030002;
+        public static final int activity_settings=0x7f030002;
+        public static final int activity_splash=0x7f030003;
     }
     public static final class menu {
         public static final int grid=0x7f070000;
         public static final int main=0x7f070001;
-        public static final int splash=0x7f070002;
+        public static final int settings=0x7f070002;
+        public static final int splash=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int alreadysign=0x7f050005;
         public static final int app_name=0x7f050000;
-        public static final int functiontodo=0x7f050008;
         public static final int hello_world=0x7f05000a;
         public static final int noonewins=0x7f050006;
+        public static final int settingsButton=0x7f050008;
         public static final int startGame1v1=0x7f050007;
         public static final int startGame1vIA=0x7f050004;
         public static final int title_activity_grid=0x7f050003;
+        public static final int title_activity_settings=0x7f05000b;
         public static final int title_activity_splash=0x7f050009;
         public static final int todo=0x7f050002;
     }
--- Tris/src/game/tris/activity/MainActivity.java
+++ Tris/src/game/tris/activity/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener, OnTouchListener{
 
@@ -22,7 +24,10 @@ public class MainActivity extends Activity implements OnClickListener, OnTouchLi
         startGame1vIA.setOnClickListener(this);
         Button startGame1v1 = (Button)findViewById(R.id.startGame1v1);
         startGame1v1.setOnClickListener(this);
-        findViewById(R.id.button1).setOnTouchListener(this);
+        Button settingsButton = (Button)findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(this);
+        
+        findViewById(R.id.settingsButton).setOnTouchListener(this);
         findViewById(R.id.startGame1v1).setOnTouchListener(this);
         findViewById(R.id.startGame1vIA).setOnTouchListener(this);
     }
@@ -41,23 +46,26 @@ public class MainActivity extends Activity implements OnClickListener, OnTouchLi
 		if(v.getId() == R.id.startGame1vIA){
 			Intent intent = new Intent(this,GridActivity.class);
 			startActivity(intent);
+		}else if(v.getId() == R.id.settingsButton){
+			Intent intent = new Intent(this,SettingsActivity.class);
+			startActivity(intent);
 		}
 	}
 
 
 	@Override
 	public boolean onTouch(View arg0, MotionEvent arg1) {
-		if(arg0.getId() == R.id.button1){
+		if(arg0.getId() == R.id.settingsButton){
 			findViewById(R.id.startGame1vIA).setPressed(false);
 			findViewById(R.id.startGame1v1).setPressed(false);
 		}
 		else if(arg0.getId() == R.id.startGame1vIA){
-			findViewById(R.id.button1).setPressed(false);
+			findViewById(R.id.settingsButton).setPressed(false);
 			findViewById(R.id.startGame1v1).setPressed(false);
 		}
 		else if(arg0.getId() == R.id.startGame1v1){
 			findViewById(R.id.startGame1vIA).setPressed(false);
-			findViewById(R.id.button1).setPressed(false);
+			findViewById(R.id.settingsButton).setPressed(false);
 		}
 		return false;
 	}
