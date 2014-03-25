5824fc4e4229190d042451f8f520bccaca6c9b17
CaoS <sosi.andrea@gmail.com>
2014-03-25 21:44:00 +0100

    fix storage/preferences in sound and difficulty

--- Tris/src/game/tris/activity/SettingsActivity.java
+++ Tris/src/game/tris/activity/SettingsActivity.java
@@ -17,28 +17,24 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class SettingsActivity extends Activity {
 
-	private RadioGroup radioGroupSound;
-	private RadioGroup radioGroupDiff;
-	SharedPreferences sharedpreferences;
-	private static Integer indexDefaultSound = 0;
-	private static Integer indexDefaultDiff = 0;
-	public static final String MyPREFERENCES = "MyPrefs" ;
-	public static final String Sound = "soundKey"; 
-	public static final String Difficulty = "diffKey";
+	RadioGroup radioGroupSound, radioGroupDiff;
+	final String SOUND = "soundKey"; 
+	final String DIFFICULTY = "diffKey";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-			
-		getRadio();
-
-		Toast.makeText(SettingsActivity.this,"Audio: "+sharedpreferences.getString(Sound, ""), Toast.LENGTH_SHORT).show();
-		Toast.makeText(SettingsActivity.this,"Difficulty: "+sharedpreferences.getString(Difficulty, ""), Toast.LENGTH_SHORT).show();	
 		
+		radioGroupSound = (RadioGroup)findViewById(R.id.radioGroup2);
+		radioGroupDiff = (RadioGroup)findViewById(R.id.radioGroup1);
+		
+		radioGroupSound.setOnCheckedChangeListener(radioSoundGroupOnCheckedChangeListener);
+		radioGroupDiff.setOnCheckedChangeListener(radioDiffGroupOnCheckedChangeListener);
 		
 		// imageview che mi collega al sito di wild stone studio
 		ImageView linksito = (ImageView)findViewById(R.id.wild);
@@ -63,92 +59,58 @@ public class SettingsActivity extends Activity {
 		        		startActivity(sendIntent);
 		            }
 	            });
-	        
-	        
+		        
+		        LoadSoundPreferences();
+		        LoadDiffPreferences();
 	}
+	
+	
+				OnCheckedChangeListener radioSoundGroupOnCheckedChangeListener = new OnCheckedChangeListener(){
+		
+		    	    @Override
+		    	    public void onCheckedChanged(RadioGroup group, int checkedId) {
+			
+			    	     RadioButton checkedRadioButton = (RadioButton)radioGroupSound.findViewById(checkedId);
+			    	     int checkedIndex = radioGroupSound.indexOfChild(checkedRadioButton);
+			    	     SavePreferences(SOUND, checkedIndex);
+	    	    }};
+	    	    
+	    	    OnCheckedChangeListener radioDiffGroupOnCheckedChangeListener = new OnCheckedChangeListener(){
+	
+	        	    @Override
+	        	    public void onCheckedChanged(RadioGroup group, int checkedId) {
+	    	
+	    	    	     RadioButton checkedRadioButton = (RadioButton)radioGroupDiff.findViewById(checkedId);
+	    	    	     int checkedIndex = radioGroupDiff.indexOfChild(checkedRadioButton);
+	    	    	     SavePreferences(DIFFICULTY, checkedIndex);
+	        	    }};
+	    	    
+    	    private void SavePreferences(String key, int value){
+    	    	SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", MODE_PRIVATE);
+    	    	SharedPreferences.Editor editor = sharedPreferences.edit();
+    	    	editor.putInt(key, value);
+    	    	editor.commit(); 
+    	    }
+    	    
+    	    private void LoadSoundPreferences(){
+    	    	  SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", MODE_PRIVATE);
+    	    	  int savedRadioIndex = sharedPreferences.getInt(SOUND, 0);
+    	    	  RadioButton savedCheckedRadioButton = (RadioButton)radioGroupSound.getChildAt(savedRadioIndex);
+    	    	  savedCheckedRadioButton.setChecked(true);
+    	    }
+    	    
+    	    private void LoadDiffPreferences(){
+  	    	  SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", MODE_PRIVATE);
+  	    	  int savedRadioIndex = sharedPreferences.getInt(DIFFICULTY, 0);
+  	    	  RadioButton savedCheckedRadioButton = (RadioButton)radioGroupDiff.getChildAt(savedRadioIndex);
+  	    	  savedCheckedRadioButton.setChecked(true);
+  	    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
-	}
-	
-	private void getRadio(){
-		
-		radioGroupSound = (RadioGroup) findViewById(R.id.radioGroup2);
-		radioGroupDiff = (RadioGroup) findViewById(R.id.radioGroup1);
-			
-		sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-		final Editor editor = sharedpreferences.edit();
-		
-		
-		if(indexDefaultSound == 0){
-			if(radioGroupSound.getCheckedRadioButtonId()!=-1){
-	            int id1= radioGroupSound.getCheckedRadioButtonId();
-	            View radioButton1 = radioGroupSound.findViewById(id1);
-	            int radioId1 = radioGroupSound.indexOfChild(radioButton1);
-	            RadioButton btn1 = (RadioButton) radioGroupSound.getChildAt(radioId1);
-	          
-	            if(btn1.getText().equals("On")){   
-	                editor.putString(Sound, "0");
-	                editor.commit();
-	            }
-			}
-			indexDefaultSound++;
-		}
-		
-		if(indexDefaultDiff == 0){
-			if(radioGroupDiff.getCheckedRadioButtonId()!=-1){
-	            int id2= radioGroupDiff.getCheckedRadioButtonId();
-	            View radioButton2 = radioGroupDiff.findViewById(id2);
-	            int radioId2 = radioGroupDiff.indexOfChild(radioButton2);
-	            RadioButton btn2 = (RadioButton) radioGroupDiff.getChildAt(radioId2);
-	          
-	            if(btn2.getText().equals("Easy")){   
-	                editor.putString(Difficulty, "0");
-	                editor.commit();
-	            }
-			}
-			indexDefaultDiff++;
-		}
-		
-		
-		 OnClickListener listener = new OnClickListener() {
-	            @Override
-	            public void onClick(View v) {
-	                   RadioButton rb = (RadioButton) v;
-	                   Toast.makeText(SettingsActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
-	                    
-	                             
-	                   if(rb.getText().equals("On")){   
-		                   editor.putString(Sound, "0");
-	                   }else if(rb.getText().equals("Off")){           	
-		                   editor.putString(Sound, "1");
-	                   }else if(rb.getText().equals("Easy")){
-	                	   editor.putString(Difficulty, "0");
-	                   }else if(rb.getText().equals("Medium")){
-	                	   editor.putString(Difficulty, "1");
-	                   }else if(rb.getText().equals("Hard")){
-	                	   editor.putString(Difficulty, "2");
-	                   }
-	                   editor.commit(); 
-	            }
-	      };
-
-	      //this are radio buttons for sound
-	      RadioButton rbOn = (RadioButton) findViewById(R.id.soundOn);
-	      rbOn.setOnClickListener(listener);
-	      RadioButton rbOff = (RadioButton) findViewById(R.id.soundOff);
-	      rbOff.setOnClickListener(listener);
-	      
-	      //this are radio buttons for difficulty
-	      RadioButton rbEasy = (RadioButton) findViewById(R.id.diffEasy);
-	      rbEasy.setOnClickListener(listener);
-	      RadioButton rbMedium = (RadioButton) findViewById(R.id.diffMedium);
-	      rbMedium.setOnClickListener(listener);
-	      RadioButton rbHard = (RadioButton) findViewById(R.id.diffHard);
-	      rbHard.setOnClickListener(listener);
 	}
 	
 	@Override
