59dd138fa140f47522f223e64b76614bc0cf3529
empy92 <empy92@hotmail.it>
2014-03-31 19:28:13 +0200

    fix grapichs

--- Tris/gen/com/example/tris/R.java
+++ Tris/gen/com/example/tris/R.java
@@ -10,11 +10,16 @@ package com.example.tris;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int click=0x7f050001;
+        public static final int color_selector=0x7f050002;
+        public static final int unclick=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
         /** 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw600dp devices (e.g. 7" tablets) here.
@@ -24,54 +29,53 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int textSize=0x7f050002;
-        public static final int textTitle=0x7f050004;
-        public static final int textlink=0x7f050003;
+        public static final int textSize=0x7f060002;
+        public static final int textTitle=0x7f060004;
+        public static final int textlink=0x7f060003;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int color_selector=0x7f020001;
-        public static final int grid=0x7f020002;
-        public static final int grid_layer=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo01=0x7f020005;
-        public static final int logo_wild=0x7f020006;
-        public static final int o=0x7f020007;
-        public static final int share=0x7f020008;
-        public static final int stella=0x7f020009;
-        public static final int x=0x7f02000a;
+        public static final int grid=0x7f020001;
+        public static final int grid_layer=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo01=0x7f020004;
+        public static final int logo_wild=0x7f020005;
+        public static final int o=0x7f020006;
+        public static final int share=0x7f020007;
+        public static final int stella=0x7f020008;
+        public static final int x=0x7f020009;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090019;
-        public static final int action_settings=0x7f09001d;
-        public static final int b00=0x7f090000;
-        public static final int b01=0x7f090002;
-        public static final int b02=0x7f090003;
-        public static final int b10=0x7f090004;
-        public static final int b11=0x7f090005;
-        public static final int b12=0x7f090006;
-        public static final int b20=0x7f090007;
-        public static final int b21=0x7f090008;
-        public static final int b22=0x7f090009;
-        public static final int background=0x7f09001b;
-        public static final int diffEasy=0x7f090013;
-        public static final int diffHard=0x7f090015;
-        public static final int diffMedium=0x7f090014;
-        public static final int grid=0x7f09001c;
-        public static final int imgLogo=0x7f09001a;
-        public static final int radioGroup1=0x7f090012;
-        public static final int radioGroup2=0x7f09000e;
-        public static final int settings=0x7f09000c;
-        public static final int share=0x7f090018;
-        public static final int soundOff=0x7f090010;
-        public static final int soundOn=0x7f09000f;
-        public static final int space=0x7f090001;
-        public static final int startGame1v1=0x7f09000b;
-        public static final int startGame1vIA=0x7f09000a;
-        public static final int textView1=0x7f09000d;
-        public static final int textView2=0x7f090011;
-        public static final int vote=0x7f090016;
-        public static final int wild=0x7f090017;
+        public static final int LinearLayout1=0x7f0a0019;
+        public static final int action_settings=0x7f0a001d;
+        public static final int b00=0x7f0a0000;
+        public static final int b01=0x7f0a0002;
+        public static final int b02=0x7f0a0003;
+        public static final int b10=0x7f0a0004;
+        public static final int b11=0x7f0a0005;
+        public static final int b12=0x7f0a0006;
+        public static final int b20=0x7f0a0007;
+        public static final int b21=0x7f0a0008;
+        public static final int b22=0x7f0a0009;
+        public static final int background=0x7f0a001b;
+        public static final int diffEasy=0x7f0a0013;
+        public static final int diffHard=0x7f0a0015;
+        public static final int diffMedium=0x7f0a0014;
+        public static final int grid=0x7f0a001c;
+        public static final int imgLogo=0x7f0a001a;
+        public static final int radioGroup1=0x7f0a0012;
+        public static final int radioGroup2=0x7f0a000e;
+        public static final int settings=0x7f0a000c;
+        public static final int share=0x7f0a0018;
+        public static final int soundOff=0x7f0a0010;
+        public static final int soundOn=0x7f0a000f;
+        public static final int space=0x7f0a0001;
+        public static final int startGame1v1=0x7f0a000b;
+        public static final int startGame1vIA=0x7f0a000a;
+        public static final int textView1=0x7f0a000d;
+        public static final int textView2=0x7f0a0011;
+        public static final int vote=0x7f0a0016;
+        public static final int wild=0x7f0a0017;
     }
     public static final class layout {
         public static final int activity_grid=0x7f030000;
@@ -80,10 +84,10 @@ public final class R {
         public static final int activity_splash=0x7f030003;
     }
     public static final class menu {
-        public static final int grid=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int settings=0x7f080002;
-        public static final int splash=0x7f080003;
+        public static final int grid=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int settings=0x7f090002;
+        public static final int splash=0x7f090003;
     }
     public static final class raw {
         public static final int buttonpress=0x7f040000;
@@ -91,21 +95,21 @@ public final class R {
         public static final int hammer=0x7f040002;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int alreadysign=0x7f060006;
-        public static final int app_name=0x7f060000;
-        public static final int company=0x7f06000e;
-        public static final int noonewins=0x7f060007;
-        public static final int settingsButton=0x7f060005;
-        public static final int settingsText1=0x7f06000b;
-        public static final int settingsText2=0x7f06000c;
-        public static final int startGame1v1=0x7f060004;
-        public static final int startGame1vIA=0x7f060003;
-        public static final int title_activity_grid=0x7f060008;
-        public static final int title_activity_settings=0x7f06000a;
-        public static final int title_activity_splash=0x7f060009;
-        public static final int todo=0x7f060002;
-        public static final int website=0x7f06000d;
+        public static final int action_settings=0x7f070001;
+        public static final int alreadysign=0x7f070006;
+        public static final int app_name=0x7f070000;
+        public static final int company=0x7f07000e;
+        public static final int noonewins=0x7f070007;
+        public static final int settingsButton=0x7f070005;
+        public static final int settingsText1=0x7f07000b;
+        public static final int settingsText2=0x7f07000c;
+        public static final int startGame1v1=0x7f070004;
+        public static final int startGame1vIA=0x7f070003;
+        public static final int title_activity_grid=0x7f070008;
+        public static final int title_activity_settings=0x7f07000a;
+        public static final int title_activity_splash=0x7f070009;
+        public static final int todo=0x7f070002;
+        public static final int website=0x7f07000d;
     }
     public static final class style {
         /** 
@@ -129,10 +133,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
